714063cbf7a151949684a2fb7adf596bf245d924
longavailable <liuxiaolong125@gmail.com>
2020-07-26 01:32:39 +0200

    v0.1.1

--- tests/01voronoiDiagram4plg.py
+++ tests/01voronoiDiagram4plg.py
@@ -1,6 +1,7 @@
+import geopandas as gpd
 from longsgis import voronoiDiagram4plg
 
 builtup = gpd.read_file('input.geojson'); builtup.crs = 32650
-boundary = gpd.read_file('boundary.geojson'); builtup.crs = 32650
+boundary = gpd.read_file('boundary.geojson'); boundary.crs = 32650
 vd = voronoiDiagram4plg(builtup, boundary)
 vd.to_file('output.geojson', driver='GeoJSON')
